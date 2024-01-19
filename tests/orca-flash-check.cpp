a819937be3f1114be1c1592b1c3b515498d48073
Alan Liddell <aliddell@chanzuckerberg.com>
2024-01-19 11:22:23 -0500

    wip

---
+++ tests/orca-flash-check.cpp
@@ -0,0 +1,108 @@
+#include "acquire.h"
+#include "platform.h"
+#include "device/hal/device.manager.h"
+#include "logger.h"
+
+#include <cstdio>
+
+#define L (aq_logger)
+#define LOG(...) L(0, __FILE__, __LINE__, __FUNCTION__, __VA_ARGS__)
+#define ERR(...) L(1, __FILE__, __LINE__, __FUNCTION__, __VA_ARGS__)
+#define CHECK(e)                                                               \
+    do {                                                                       \
+        if (!(e)) {                                                            \
+            ERR("Expression evaluated as false: " #e);                         \
+            goto Error;                                                        \
+        }                                                                      \
+    } while (0)
+#define DEVOK(e) CHECK(Device_Ok == (e))
+#define OK(e) CHECK(AcquireStatus_Ok == (e))
+
+#define countof(e) (sizeof(e) / sizeof(*(e)))
+
+void
+reporter(int is_error,
+         const char* file,
+         int line,
+         const char* function,
+         const char* msg)
+{
+    fprintf(is_error ? stderr : stdout,
+            "%s%s(%d) - %s: %s\n",
+            is_error ? "ERROR " : "",
+            file,
+            line,
+            function,
+            msg);
+}
+
+int
+main()
+{
+
+    int ecode = 1;
+    AcquireRuntime* runtime = 0;
+    AcquireProperties props = {};
+
+    AcquirePropertyMetadata meta = { 0 };
+
+    CHECK(runtime = acquire_init(reporter));
+    OK(acquire_get_configuration(runtime, &props));
+
+    // Select devices
+    {
+        const DeviceManager* dm;
+#define SIZED(s) s, sizeof(s) - 1
+        CHECK(dm = acquire_device_manager(runtime));
+        DEVOK(device_manager_select(dm,
+                                    DeviceKind_Camera,
+                                    SIZED("Hamamatsu C13440-20C.*"),
+                                    &props.video[0].camera.identifier));
+        DEVOK(device_manager_select(dm,
+                                    DeviceKind_Storage,
+                                    SIZED("Trash"),
+                                    &props.video[0].storage.identifier));
+#undef SIZED
+    }
+
+    // Configure
+    {
+        props.video[0].max_frame_count = 1;
+        props.video[0].frame_average_count = 0;
+        props.video[0].camera.settings.pixel_type = SampleType_u16;
+        props.video[0].camera.settings.shape = { .x = 640, .y = 480 };
+        OK(acquire_configure(runtime, &props));
+    }
+    OK(acquire_get_configuration_metadata(runtime, &meta));
+    {
+        const auto* info = &meta.video[0].camera.digital_lines;
+        for (int i = 0; i < info->line_count; ++i)
+            LOG("Digital line %d: %s", i, info->names[i]);
+    }
+
+    // Run
+    {
+        OK(acquire_start(runtime));
+        clock_sleep_ms(0, 1000);
+        OK(acquire_stop(runtime));
+    }
+
+#if 0
+    // Check acquired data
+    {
+        struct VideoFrame* frames;
+        size_t bytes_of_frames;
+        OK(acquire_map_read(runtime, &frames, &bytes_of_frames));
+        CHECK(frames);
+        CHECK(bytes_of_frames == 640 * 480 * 2);
+    }
+#endif
+
+    ecode = 0;
+Finalize:
+    acquire_shutdown(runtime);
+    return ecode;
+Error:
+    ecode = 1;
+    goto Finalize;
+}
--- tests/orca-flash-one-video-stream.cpp
+++ tests/orca-flash-one-video-stream.cpp
@@ -45,34 +45,6 @@ reporter(int is_error,
 #define OK(e) CHECK(AcquireStatus_Ok == (e))
 
 int
-camera_select(const DeviceManager* dm, AcquireProperties* props)
-{
-    try {
-        CHECK(dm);
-        CHECK(props);
-
-        CHECK(Device_Ok ==
-                device_manager_select(dm,
-                                      DeviceKind_Camera,
-                                      SIZED("Hamamatsu C15440-20UP.*"),
-                                      &props->video[0].camera.identifier) ||
-              Device_Ok ==
-                device_manager_select(dm,
-                                      DeviceKind_Camera,
-                                      SIZED("Hamamatsu C13440-20C.*"),
-                                      &props->video[0].camera.identifier));
-
-        return 1;
-    } catch (const std::exception& e) {
-        ERR("Exception: %s", e.what());
-    } catch (...) {
-        ERR("Exception: (unknown)");
-    }
-
-    return 0;
-}
-
-int
 main()
 {
 
@@ -85,7 +57,10 @@ main()
         AcquireProperties props = {};
         OK(acquire_get_configuration(runtime, &props));
 
-        CHECK(camera_select(dm, &props));
+        DEVOK(device_manager_select(dm,
+                                    DeviceKind_Camera,
+                                    SIZED("Hamamatsu C13440-20C.*") - 1,
+                                    &props.video[0].camera.identifier));
         DEVOK(device_manager_select(dm,
                                     DeviceKind_Storage,
                                     SIZED("tiff") - 1,
@@ -94,6 +69,7 @@ main()
         storage_properties_init(
           &props.video[0].storage.settings, 0, SIZED("out.tif"), 0, 0, { 0 });
 
+        props.video[0].camera.settings.readout_direction = Direction_Forward;
         OK(acquire_configure(runtime, &props));
 
         AcquirePropertyMetadata metadata = { 0 };
